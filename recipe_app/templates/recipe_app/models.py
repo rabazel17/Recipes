bb268605b22b36ee069df1dfe0ce5f71cfbd6d91
Natasha Gurova <rabazel17@mail.ru>
2024-02-27 10:34:51 +0300

    decision

---
+++ recipe_app/templates/recipe_app/models.py
@@ -0,0 +1,34 @@
+# from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
+from django.db import models
+
+class CathegoryModel(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.title}'
+
+class RecipeModel(models.Model):
+    COMPLEXITY = [
+        ('easy', 'easy'),
+        ('medium', 'medium'),
+        ('complex', 'complex'),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    cooking =  models.TextField()
+    time = models.CharField(max_length=20)
+    image = models.ImageField()
+    author = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    cathegories = models.ManyToManyField(CathegoryModel, default='new')
+    complexity = models.CharField(max_length = 10, choices=COMPLEXITY)
+
+    def __str__(self):
+        return f'{self.title}'
+    
+
+
+class RecipeToCategoryModel(models.Model):
+    recipe = models.ForeignKey(RecipeModel, on_delete=models.CASCADE)
+    cathegory = models.ForeignKey(CathegoryModel, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
